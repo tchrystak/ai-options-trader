dc46b4d17e2a92fd5a04e61c6c464d44ad0f16cb
tchrystak <chrystakcodes@gmail.com>
2026-08-20 00:27:30 -0500

    Connect 8:45 scheduler to paper trade

--- src/scheduler/marketClock.js
+++ src/scheduler/marketClock.js
@@ -1,8 +1,10 @@
+import { placePaperTrade } from "../trading/paperTrade.js";
+
 let lastTradeDate = null; // The starting state is nothing is executed
 
 setInterval(() => {
-
   const now = new Date();
+  console.log("Checking the market clock...");
 
   // Get the date
   const tradingDate = new Intl.DateTimeFormat("en-US", {
@@ -17,7 +19,7 @@ setInterval(() => {
     timeZone: "America/Chicago",
     hour: "numeric",
     hour12: false,
-  }).format(now); 
+  }).format(now);
 
   // Get the current minute in Central Time
   const minute = new Intl.DateTimeFormat("en-US", {
@@ -32,9 +34,20 @@ setInterval(() => {
   // Is it 8:45?
   const isTradeTime = hourNumber === 8 && minuteNumber === 45;
 
-  // Should we execute today's trade yet? 
+  // Should we execute today's trade yet?
   if (isTradeTime && tradingDate !== lastTradeDate) {
     console.log("It's 8:45 AM — execute trade!");
+
+    const paperTrade = placePaperTrade(
+      "NVDA",
+      "PUT",
+      180,
+      "2026-08-21",
+      1,
+      2.5,
+    );
+
+    console.log(paperTrade);
 
     lastTradeDate = tradingDate;
   }
